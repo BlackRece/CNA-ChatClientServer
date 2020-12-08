175b500e69f54482e5892ed1903ff489a0e08d48
BlackRece <mauricemlth@hotmail.com>
2020-12-08 18:19:38 +0000

    updated/fix UI/UX design of both WPF and WIN forms

--- ClientServerTutorial/Client/Client_WinForm.Designer.cs
+++ ClientServerTutorial/Client/Client_WinForm.Designer.cs
@@ -26,65 +26,80 @@
             this.MessageWindow = new System.Windows.Forms.TextBox();
             this.InputField = new System.Windows.Forms.TextBox();
             this.SubmitButton = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.NickName_label = new System.Windows.Forms.Label();
             this.ChangeName_Button = new System.Windows.Forms.Button();
             this.ChangeName_textbox = new System.Windows.Forms.TextBox();
-            this.groupBox1.SuspendLayout();
+            this.UserList = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Clear_Selected = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // MessageWindow
             // 
-            this.MessageWindow.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.SetColumnSpan(this.MessageWindow, 2);
+            this.MessageWindow.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MessageWindow.Location = new System.Drawing.Point(273, 10);
+            this.MessageWindow.Margin = new System.Windows.Forms.Padding(10);
             this.MessageWindow.Multiline = true;
             this.MessageWindow.Name = "MessageWindow";
             this.MessageWindow.ReadOnly = true;
-            this.MessageWindow.Size = new System.Drawing.Size(776, 358);
+            this.tableLayoutPanel1.SetRowSpan(this.MessageWindow, 3);
+            this.MessageWindow.Size = new System.Drawing.Size(501, 356);
             this.MessageWindow.TabIndex = 0;
+            this.MessageWindow.Text = "Received Messages";
             // 
             // InputField
             // 
-            this.InputField.Location = new System.Drawing.Point(12, 418);
+            this.InputField.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.InputField.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.InputField.Location = new System.Drawing.Point(273, 381);
+            this.InputField.Margin = new System.Windows.Forms.Padding(10, 0, 10, 10);
+            this.InputField.MinimumSize = new System.Drawing.Size(400, 20);
             this.InputField.Name = "InputField";
-            this.InputField.Size = new System.Drawing.Size(695, 20);
+            this.InputField.Size = new System.Drawing.Size(400, 20);
             this.InputField.TabIndex = 1;
             // 
             // SubmitButton
             // 
-            this.SubmitButton.Location = new System.Drawing.Point(713, 415);
+            this.SubmitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SubmitButton.AutoSize = true;
+            this.SubmitButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.SubmitButton.Location = new System.Drawing.Point(683, 376);
+            this.SubmitButton.Margin = new System.Windows.Forms.Padding(0, 0, 10, 10);
+            this.SubmitButton.MinimumSize = new System.Drawing.Size(90, 20);
             this.SubmitButton.Name = "SubmitButton";
-            this.SubmitButton.Size = new System.Drawing.Size(75, 23);
+            this.SubmitButton.Size = new System.Drawing.Size(91, 25);
             this.SubmitButton.TabIndex = 2;
             this.SubmitButton.Text = "Submit";
             this.SubmitButton.UseVisualStyleBackColor = true;
             this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
             // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.NickName_label);
-            this.groupBox1.Controls.Add(this.ChangeName_Button);
-            this.groupBox1.Controls.Add(this.ChangeName_textbox);
-            this.groupBox1.Location = new System.Drawing.Point(12, 376);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(363, 36);
-            this.groupBox1.TabIndex = 3;
-            this.groupBox1.TabStop = false;
-            // 
             // NickName_label
             // 
-            this.NickName_label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.NickName_label.AutoSize = true;
-            this.NickName_label.Location = new System.Drawing.Point(70, 12);
+            this.NickName_label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tableLayoutPanel1.SetColumnSpan(this.NickName_label, 2);
+            this.NickName_label.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.NickName_label.Location = new System.Drawing.Point(10, 376);
+            this.NickName_label.Margin = new System.Windows.Forms.Padding(10, 0, 10, 10);
             this.NickName_label.Name = "NickName_label";
-            this.NickName_label.Size = new System.Drawing.Size(53, 13);
+            this.NickName_label.Size = new System.Drawing.Size(243, 25);
             this.NickName_label.TabIndex = 9;
             this.NickName_label.Text = "nickname";
+            this.NickName_label.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // ChangeName_Button
             // 
-            this.ChangeName_Button.Location = new System.Drawing.Point(282, 7);
+            this.ChangeName_Button.AutoSize = true;
+            this.ChangeName_Button.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ChangeName_Button.Location = new System.Drawing.Point(182, 344);
+            this.ChangeName_Button.Margin = new System.Windows.Forms.Padding(10, 0, 10, 10);
             this.ChangeName_Button.Name = "ChangeName_Button";
-            this.ChangeName_Button.Size = new System.Drawing.Size(75, 23);
+            this.ChangeName_Button.Size = new System.Drawing.Size(71, 22);
             this.ChangeName_Button.TabIndex = 8;
             this.ChangeName_Button.Text = "Change Nick";
             this.ChangeName_Button.UseVisualStyleBackColor = true;
@@ -92,26 +107,86 @@
             // 
             // ChangeName_textbox
             // 
-            this.ChangeName_textbox.Location = new System.Drawing.Point(129, 7);
+            this.ChangeName_textbox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ChangeName_textbox.Location = new System.Drawing.Point(10, 344);
+            this.ChangeName_textbox.Margin = new System.Windows.Forms.Padding(10, 0, 10, 10);
             this.ChangeName_textbox.Name = "ChangeName_textbox";
-            this.ChangeName_textbox.Size = new System.Drawing.Size(147, 20);
+            this.ChangeName_textbox.Size = new System.Drawing.Size(152, 20);
             this.ChangeName_textbox.TabIndex = 7;
+            this.ChangeName_textbox.Text = "Change name TextBox";
+            this.ChangeName_textbox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // UserList
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.UserList, 2);
+            this.UserList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.UserList.FormattingEnabled = true;
+            this.UserList.Location = new System.Drawing.Point(10, 10);
+            this.UserList.Margin = new System.Windows.Forms.Padding(10);
+            this.UserList.Name = "UserList";
+            this.UserList.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.UserList.Size = new System.Drawing.Size(243, 292);
+            this.UserList.Sorted = true;
+            this.UserList.TabIndex = 4;
+            this.UserList.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 47.27995F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.27472F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 27.47253F));
+            this.tableLayoutPanel1.Controls.Add(this.SubmitButton, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.MessageWindow, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.UserList, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.ChangeName_Button, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.ChangeName_textbox, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.InputField, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.NickName_label, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.Clear_Selected, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 76F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(784, 411);
+            this.tableLayoutPanel1.TabIndex = 10;
+            // 
+            // Clear_Selected
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.Clear_Selected, 2);
+            this.Clear_Selected.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Clear_Selected.Location = new System.Drawing.Point(10, 312);
+            this.Clear_Selected.Margin = new System.Windows.Forms.Padding(10, 0, 10, 10);
+            this.Clear_Selected.Name = "Clear_Selected";
+            this.Clear_Selected.Size = new System.Drawing.Size(243, 22);
+            this.Clear_Selected.TabIndex = 10;
+            this.Clear_Selected.Text = "Clear Selected Users";
+            this.Clear_Selected.UseVisualStyleBackColor = true;
             // 
             // Client_WinForm
             // 
             this.AcceptButton = this.SubmitButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.SubmitButton);
-            this.Controls.Add(this.InputField);
-            this.Controls.Add(this.MessageWindow);
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(784, 411);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(800, 450);
             this.Name = "Client_WinForm";
             this.Text = "Client_WinForm";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Client_WinForm_FormClosing);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,9 +197,11 @@
         private System.Windows.Forms.TextBox MessageWindow;
         private System.Windows.Forms.TextBox InputField;
         private System.Windows.Forms.Button SubmitButton;
-        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button ChangeName_Button;
         private System.Windows.Forms.TextBox ChangeName_textbox;
         private System.Windows.Forms.Label NickName_label;
+        private System.Windows.Forms.ListBox UserList;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button Clear_Selected;
     }
 }
